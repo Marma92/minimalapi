53064d25d804ea91ff1c9316278060c28e5493e7
Amram <a.elbaz@adexgroup.fr>
2018-04-16 10:26:05 +0000

    cors

--- routes.js
+++ routes.js
@@ -2,23 +2,24 @@ var fs = require("fs");
 var util = require("util");
 
 module.exports = function(app){
-  app.get('/', function(req, res) {
+
+	app.get('/', function(req, res, next) {
 		res.json({message : "Envoyez un fichier", method : req.method});
-  });
-  app.post('/', function(req, res) {
+ 	 });
+  app.post('/', function(req, res, next) {
+	console.log(req.body);
     if (!req.is('application/json')) {
        res.json({message : "erreur d'envoi", methode : req.method});
        res.sendStatus(400);
     }
     else {
-      console.log(req);
       fs.writeFile('input.json', JSON.stringify(req.body, null, "   "),  function(err) {
          if (err) {
             return console.error(err);
          }
          console.log("Data written successfully!");
       });
-      res.json({message : "fichier reçu", methode : req.method});
+      res.json({message : "fichier reçu", method : req.method});
     }
   });
 	app.get('*', function(req, res){
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express     = require('express');
+var cors 	= require('cors');
 var app         = express();
 var bodyParser  = require('body-parser');
 var port        = process.env.PORT || 3000;
 var morgan      = require('morgan');
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true }));
